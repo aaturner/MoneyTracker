f4ba093a46500ce08a5727b690572bbbe6b479b7
Aaron Turner <aarondavidturner@hotmail.com>
2017-07-19 20:00:48 -0600

    css update, add sys button

--- MoneyTracker/App_Start/BundleConfig.cs
+++ MoneyTracker/App_Start/BundleConfig.cs
@@ -24,7 +24,7 @@ namespace MoneyTracker
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/Slate_bootstrap.css",
+                      "~/Content/Superhero_bootstrap.css",
                       "~/Content/site.css"));
         }
     }
